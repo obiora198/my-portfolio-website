2b16b8f89158b2eaf0e2defcda86c099ce1a62d4
Emmanuel Obiora <emmanuelobiora11@gmail.com>
2025-04-13 21:47:12 +0100

    fix firebase image host error and apostrophy error

--- app/components/sections/Projects.tsx
+++ app/components/sections/Projects.tsx
@@ -29,13 +29,13 @@ export default function Projects() {
         <h1 className="text-4xl font-bold text-indigo-500 inline-block text-center border-b-2 my-4">
           Projects
         </h1>
-        <div className="w-full columns-2 gap-8">
-          {projects ? projects.map((project) => (
-            <div
-              key={project.id}
-              className="w-full bg-white flex flex-col gap-3 items-center justify-center p-8 border-2 rounded-[32px] break-inside-avoid-column mb-8"
-            >
-              
+        <div className="w-full columns-2 gap-8 relative">
+          {projects ? (
+            projects.map((project) => (
+              <div
+                key={project.id}
+                className="w-full bg-white flex flex-col gap-3 items-center justify-center p-8 border-2 rounded-[32px] break-inside-avoid-column mb-8"
+              >
                 <Image
                   width={300}
                   height={350}
@@ -47,15 +47,28 @@ export default function Projects() {
                   <h2 className="mt-2 text-2xl font-bold">
                     {project.data.title}
                   </h2>
-                  <div className='w-full flex items-center justify-center gap-4'>
-                    <a className='text-l px-8 py-2 rounded-full border-2 font-bold hover:bg-indigo-500 hover:text-white transition-all duration-500' href={project.data.link}>Live Demo</a>
-                    <a className='text-l px-8 py-2 rounded-full border-2 font-bold hover:bg-indigo-500 hover:text-white transition-all duration-500' href={project.data.githubLink}>Code</a>
+                  <div className="w-full flex items-center justify-center gap-4">
+                    <a
+                      className="text-l px-8 py-2 rounded-full border-2 font-bold hover:bg-indigo-500 hover:text-white transition-all duration-500"
+                      href={project.data.link}
+                    >
+                      Live Demo
+                    </a>
+                    <a
+                      className="text-l px-8 py-2 rounded-full border-2 font-bold hover:bg-indigo-500 hover:text-white transition-all duration-500"
+                      href={project.data.githubLink}
+                    >
+                      Code
+                    </a>
                   </div>
                 </div>
+              </div>
+            ))
+          ) : (
+            <div className="w-full h-[300px] flex items-center justify-center absolute top-1/2">
+              <div className="w-28 h-28 border-4 border-white rounded-full animate-spin border-t-indigo-600 inline-block"></div>
             </div>
-          )) :
-          (<Loading dark={null} />)
-          }
+          )}
         </div>
       </div>
     </section>
